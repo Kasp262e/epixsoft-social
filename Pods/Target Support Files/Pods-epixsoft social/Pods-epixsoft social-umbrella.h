ffa943cd98e8afe27b289e16793cf76c02006c25
Kasp262e <kasperskovhansen@gmail.com>
2017-02-04 19:58:05 +0100

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-epixsoft social/Pods-epixsoft social-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_epixsoft_socialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_epixsoft_socialVersionString[];
+
---
+++ epixsoft social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  epixsoft social
+//
+//  Created by Kasper Hansen on 04/02/2017.
+//  Copyright © 2017 EpixSoft. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
